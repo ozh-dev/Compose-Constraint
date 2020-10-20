0b47eef8eff3aaa047de2ee9478b37286e3e77e0
Олег <zhilo@surfstudio.ru>
2020-10-20 21:10:03 +0300

    init commit

---
+++ app/src/main/java/ru/surf/constraint/MainActivity.kt
@@ -0,0 +1,38 @@
+package ru.surf.constraint
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import ru.surf.constraint.ui.ComposeConstraintTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeConstraintTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeConstraintTheme {
+        Greeting("Android")
+    }
+}
